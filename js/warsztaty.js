3d6af0a68cc5d46ee0b80476cef1b91c1a375d1b
Paulina <paulaa1530@gmail.com>
2017-10-21 16:28:18 +0200

    done zad 2

--- js/warsztaty.js
+++ js/warsztaty.js
@@ -5,6 +5,7 @@ console.log ("działa")
 document.addEventListener('DOMContentLoaded', function() {
       // tutaj będziemy umieszczać kod ze wszystkich zadań
       //Mam 100% pewności, że caly html sie wczytal
+//Zadanie 1
 
   var menuList = document.querySelectorAll(".nav-list > li"); //wyszukaj mi tylko bezpośrednie dzieci
   console.log(menuList);  /// mamy tutaj tablice!!!
@@ -36,9 +37,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
     })
   }
+  /// Zadanie 2
+  var buttons = document.querySelectorAll('.read-more');
+    //console.log(buttons);
 
+      //console.log(buttons[0].previousElementSibling); // Powinnaś zobaczyć w konsoli paragraf o klasie `more`
+ 
+ 
+     //console.log(this); // Powinnaś zobaczyć przycisk w konsoli
 
+      
+      buttons[0].addEventListener('click', function() {
+      	var textArea = this.previousElementSibling;
 
+      // Poniższa linijka sprawdza, czy `style.display` jest ustawiony na `none` LUB jest pusty
+      // Dwie kreski obok siebie, czyli || oznaczają LUB, więc aby wykonał się kod podany w nawiasie klamrowym
+      // warunek po jednej albo po drugiej stronie kresek musi być spełniony
 
+      if (textArea.style.display === 'none' || textArea.style.display === '') {
+
+        // ten kod wykona się, gdy spełni się jeden z powyższych warunków
+        textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+
+        // ten kod zadziała, gdy warunek podany w `if` nie będzie spełniony, czyli w przypadku
+        // gdy `style.display` jest inny niż `none` oraz gdy nie jest pusty
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      }
 
     });
+
+    });
+    
+
+
+    
